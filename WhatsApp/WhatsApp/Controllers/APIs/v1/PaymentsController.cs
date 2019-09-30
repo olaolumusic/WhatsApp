00e91f0583044e8aa2198c500545631ee52c7bbb
Olaolu Ajose <41782115+olaolu-ajose@users.noreply.github.com>
2019-09-30 17:06:22 +0100

    Area configurations Controller setup

--- WhatsApp/WhatsApp/Controllers/APIs/v1/PaymentsController.cs
+++ WhatsApp/WhatsApp/Controllers/APIs/v1/PaymentsController.cs
@@ -1,37 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
-namespace WhatsApp.Controllers
+namespace WhatsApp.Controllers.APIs.v1
 {
-    public class ValuesController : ApiController
+    public class PaymentsController : ApiController
     {
-        // GET api/values
+        // GET: api/Payments
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET: api/Payments/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Payments
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Payments/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/Payments/5
         public void Delete(int id)
         {
         }
