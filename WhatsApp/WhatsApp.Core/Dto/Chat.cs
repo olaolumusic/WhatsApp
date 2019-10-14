673327b9906b8a30edcb4c2cd539bc4964e4fd05
Tim Akinsola <akinsola42@gmail.com>
2019-10-14 18:42:13 +0100

    Data Objects foe Chat and User

---
+++ WhatsApp/WhatsApp.Core/Dto/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsApp.Dto.Core
+{
+    public class Chat
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string ChatType { get; set; } //Private or Group
+        public string Message { get; set; }
+        public string MessageInfo { get; set; }
+        public DateTime DateOfChat { get; set; }
+    }
+}
