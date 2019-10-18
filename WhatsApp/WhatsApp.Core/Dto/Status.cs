d1a3f8deccff10b05250eef0cb2bb4a92b9d1e44
Tim Akinsola <akinsola42@gmail.com>
2019-10-18 12:17:39 +0100

    More classes

---
+++ WhatsApp/WhatsApp.Core/Dto/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsApp.Dto.Core
+{
+    public class Status
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public DateTime StatusDate { get; set; }
+        public string StatusType { get; set; }
+    }
+}
