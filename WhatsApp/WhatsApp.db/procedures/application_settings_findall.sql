253cf9a39776c60d11c56208e340f4dd7c24bbef
Tim Akinsola <akinsola42@gmail.com>
2019-10-31 03:30:49 +0100

    Application Log fields

---
+++ WhatsApp/WhatsApp.db/procedures/application_settings_findall.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[application_settings_findall]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
--- WhatsApp/WhatsApp.db/tables/application_logs.sql
+++ WhatsApp/WhatsApp.db/tables/application_logs.sql
@@ -1,13 +1,7 @@
 ﻿CREATE TABLE [dbo].[application_logs]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(500) NOT NULL, 
-    [PhoneNumber] BIGINT NULL, 
-    [State] NVARCHAR(200) NULL, 
-    [Country] NVARCHAR(200) NULL, 
-    [DeviceName] NVARCHAR(200) NULL, 
-    [TimeDuration] DATETIME NULL, 
-    [UserAgent] NVARCHAR(500) NULL
+	[LogId] BIGINT NOT NULL PRIMARY KEY, 
+    [LogMessage] NVARCHAR(1000) NULL, 
+    [StackTrace] NVARCHAR(1000) NULL, 
+    [LogDate] DATETIME NULL
 )
-
-
